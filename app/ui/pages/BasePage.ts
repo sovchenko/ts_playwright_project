e3faa36c976f2125da20908fbcab96ca635b6124
Serhii Ovchenko <serhii_ovchenko@epam.com>
2021-11-02 16:45:32 +0200

    removed commented properties

--- app/ui/pages/BasePage.ts
+++ app/ui/pages/BasePage.ts
@@ -1,8 +1,6 @@
 import { Browser, chromium, Page } from 'playwright'
 
 export class BasePage {
-    // browser!: Browser;
-    // page!: Page;
 
     async openHomePage(page: Page): Promise<BasePage> {
         //using base url for the page
